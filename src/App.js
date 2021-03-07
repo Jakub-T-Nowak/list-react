3ab5031f0423c93bf49f9bc6fa7b6d1ba82fbf61
Jakub Nowak <Jnow87@gmail.com>
2021-03-07 21:21:50 +0100

    sorting users

--- src/App.js
+++ src/App.js
@@ -8,14 +8,29 @@ export default class App extends Component {
 
   async componentDidMount () {
     const response = await axios.get('https://teacode-recruitment-challenge.s3.eu-central-1.amazonaws.com/users.json',
-    {headers: {"Access-Control-Allow-Origin": "*"}})
-    this.setState({users: response.data})
+      {headers: {"Access-Control-Allow-Origin": "*"}}
+    );
+    const users = response.data.sort(this.sortx);
+
+    this.setState({users})
+  }
+
+  sortx = (a, b) => {
+    var nameA = a.last_name.toUpperCase();
+    var nameB = b.last_name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
   }
 
   render () {
     return (
       <div>
-        {this.state.users.map((user, index) => <div key={index}>{user.first_name} </div>)}
+        {this.state.users.map((user, index) => <div key={index}>{user.last_name} </div>)}
       </div>
     )
   }
