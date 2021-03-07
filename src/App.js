634d6df164588c7190c5807fc8ddb278cc02dac2
Jakub Nowak <Jnow87@gmail.com>
2021-03-07 21:07:46 +0100

    cleaning + axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    users: []
+  }
+
+  async componentDidMount () {
+    const response = await axios.get('https://teacode-recruitment-challenge.s3.eu-central-1.amazonaws.com/users.json',
+    {headers: {"Access-Control-Allow-Origin": "*"}})
+    this.setState({users: response.data})
+  }
+
+  render () {
+    return (
+      <div>
+        {this.state.users.map((user, index) => <div key={index}>{user.first_name} </div>)}
+      </div>
+    )
+  }
 }
-
-export default App;
