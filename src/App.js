9ca0dc603f81c6e2d920a2dd91db70e81111f3e0
Jakub Nowak <Jnow87@gmail.com>
2021-03-07 21:46:02 +0100

    filter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, {Component} from 'react';
+import Filter from './Filter';
 import axios from 'axios';
 
 export default class App extends Component {
   state = {
-    users: []
+    users: [],
+    filteredUsers: []
   }
 
   async componentDidMount () {
     const response = await axios.get('https://teacode-recruitment-challenge.s3.eu-central-1.amazonaws.com/users.json',
       {headers: {"Access-Control-Allow-Origin": "*"}}
     );
-    const users = response.data.sort(this.sortx);
-
-    this.setState({users})
+    const users = response.data.sort(this.compareNames);
+    this.setState({users, filteredUsers: users})
   }
 
-  sortx = (a, b) => {
+  compareNames = (a, b) => {
     var nameA = a.last_name.toUpperCase();
     var nameB = b.last_name.toUpperCase();
     if (nameA < nameB) {
@@ -27,10 +28,19 @@ export default class App extends Component {
     return 0;
   }
 
+  filter = filterx => {
+    var users = this.state.users
+    users = users.filter(user =>
+      user.last_name.toLowerCase().includes(filterx.toLowerCase()) || user.first_name.toLowerCase().includes(filterx.toLowerCase())
+    )
+    this.setState({filteredUsers: users});
+  }
+
   render () {
     return (
       <div>
-        {this.state.users.map((user, index) => <div key={index}>{user.last_name} </div>)}
+        <Filter filter={this.filter}/>
+        {this.state.filteredUsers.map((user, index) => <div key={index}>{user.last_name} </div>)}
       </div>
     )
   }
